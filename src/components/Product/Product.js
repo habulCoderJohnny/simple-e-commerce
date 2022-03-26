a7dd61f046b7721f4259f63d0028ecef025eec29
habulCoderJohnny <fokruljohnny@gmail.com>
2022-03-26 16:26:29 +0600

    price property added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './Product.css';
 const Product = (props) => {
     const {product}= props;
-    const {title,img} = product;
+    const {title,img,price} = product;
  
     return (
         <div className='product-list'>
             <img src={img} alt=""></img>
           <div className='product-info'>
             <p className='product-name'>{title}</p>
-
+            <p className='price'>Price : ${price}</p>
            </div> 
         </div>
     );
