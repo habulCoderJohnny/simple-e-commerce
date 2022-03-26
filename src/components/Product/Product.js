c9f78e6b488a9dd0e53c27064c52446f0e93e9cf
habulCoderJohnny <fokruljohnny@gmail.com>
2022-03-26 16:25:32 +0600

    title property added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './Product.css';
 const Product = (props) => {
     const {product}= props;
-    const {img} = product;
+    const {title,img} = product;
  
     return (
         <div className='product-list'>
             <img src={img} alt=""></img>
           <div className='product-info'>
+            <p className='product-name'>{title}</p>
 
            </div> 
         </div>
